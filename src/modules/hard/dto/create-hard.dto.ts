aedf11d5338dc0d1a470df01e012976a1d4d0575
Jimmy Lopez <jimmylo16@gmail.com>
2023-12-21 16:46:34 -0500

    Add More docs

--- src/modules/hard/dto/create-hard.dto.ts
+++ src/modules/hard/dto/create-hard.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class CreateHardDto {
   @IsString()
   @IsOptional()
+  @ApiProperty()
   file?: string;
   @IsString()
   @IsOptional()
+  @ApiProperty()
   url?: string;
 }
